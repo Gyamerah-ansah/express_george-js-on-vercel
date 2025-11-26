d75bff7aa87c9787a86eb4286b5a3b6bd0e2532c
Gyamerah-ansah <ansahg718@gmail.com>
2025-11-26 12:03:29 -0800

    push

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+
+    username:String,
+    email:String,
+    password:String
+
+}) // Replace with real DB in production
+
+
+
+
+module.exports = mongoose.model("User",userSchema);
